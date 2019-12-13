50de62f020460945ec5c133f4f9b456a1416ac17
agaverdi <adil.agaverdiyev@mail.ru>
2019-12-13 17:19:41 +0400

    5ulduz second melimin seevimli telebesi zad

--- django_solution/myapp/views.py
+++ django_solution/myapp/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+import requests
+import random
 # Create your views here.
 def owlcarousel(request):
     return render(request ,'basic.html')
+
+
+    
+def news(request):
+    news_url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=74c3bda5e80940128f54741ba6816686'
+    r = requests.get(news_url)
+    total_news = r.json()['totalResults']
+    news_number = random.randrange(0,total_news)
+    title = r.json()['articles'][news_number]['title']
+    author = r.json()['articles'][news_number]['author']
+    content = r.json()['articles'][news_number]['content']
+    image = r.json()['articles'][news_number]['urlToImage']
+    published = r.json()['articles'][news_number]['publishedAt']
+    context = {
+        'title': title,
+        'author': author,
+        'content': content,
+        'image': image,
+        'published': published,
+    }
+    return render(request, 'news.html', context)
--- django_solution/static_project/urls.py
+++ django_solution/static_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import owlcarousel
+from myapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('owlcarousel/', owlcarousel),
+    path('random-news/', news),
 ]
